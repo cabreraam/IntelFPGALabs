4824c80f19c947a55eb0c6bd12ada47eb70a427c
Anthony Cabrera <cabreraam33@gmail.com>
2019-05-26 16:49:07 -0500

    initial work on Lab8 Part1

---
+++ Lab8/Part1/hdl/top_ram.v
@@ -0,0 +1,17 @@
+module top_ram(
+	input [4:0] addr,
+	input clk,
+	input [3:0] data,
+	input wr_en,
+	output q	
+);
+
+	ram32x4 my_ram(
+		.address(addr), //input
+		.clock(clk), //input
+		.data(data), //input
+		.wren(wr_en), //input
+		.q(q) //output
+	);
+
+endmodule 
